16ca7cc6b44198e9109b13346a9980820c10d0d2
LAPTOP-QC0MP4M8\USER <lionsroar@khu.ac.kr>
2021-10-08 10:33:30 +0900

    add multiplication table

---
+++ multiplication_table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    
+    printf("* 구구단 *\n");
+    for(i = 2; i < 9; i++) {
+        printf("- %d 단 -\n", i);
+        for(j = 1; j <= 9; j++) {
+            printf("%d * %d = %d\n", i, j, i*j);
+        }
+        printf("\n");
+    }
+}
