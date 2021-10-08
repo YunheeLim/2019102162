7d1b4c5413af35cb5b3ba242de09115946a591ef
Yunhee Lim <lyhhh0320@gmail.com>
2021-10-08 10:50:31 +0900

    9단 추가

--- multiplication_table.c
+++ multiplication_table.c
@@ -5,7 +5,7 @@ int main()
     int i, j;
     
     printf("* 구구단 *\n");
-    for(i = 2; i < 9; i++) {
+    for(i = 2; i <= 9; i++) {
         printf("- %d 단 -\n", i);
         for(j = 1; j <= 9; j++) {
             printf("%d * %d = %d\n", i, j, i*j);
